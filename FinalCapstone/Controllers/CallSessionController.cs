8481b466740212704439bf2cc33d07ede25bb36d
unknown <buckleyderekp@gmail.com>
2020-07-25 12:50:01 -0500

    callsession repository, controller, prodicer(incomplete)

---
+++ FinalCapstone/Controllers/CallSessionController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalCapstone.Data;
+using FinalCapstone.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalCapstone.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallSessionController : ControllerBase
+    {
+        private readonly CallSessionRepository _callSessionRepo;
+        public CallSessionController(ApplicationDBContext context)
+        {
+            _callSessionRepo = new CallSessionRepository(context);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var callSessions = _callSessionRepo.GetById(id);
+            if (callSessions == null)
+            {
+                return NotFound();
+            }
+            return Ok(callSessions);
+        }
+
+
+
+    }
+}
---
+++ FinalCapstone/Repositories/CallSessionRepository.cs
@@ -0,0 +1,27 @@
+﻿using FinalCapstone.Data;
+using FinalCapstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalCapstone.Repositories
+{
+    public class CallSessionRepository
+    {
+        private readonly ApplicationDBContext _context;
+
+        public CallSessionRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public List<CallSession> GetById(int id)
+        {
+            return _context.CallSession
+                            .Where(cs => cs.UserProfileId == id)
+                            .OrderByDescending(cs => cs.Date) 
+                            .ToList();
+        }
+    }
+}
